c4edb38102530834a66b578d26c2b453b60df134
yeshuoer <xlsapple@outlook.com>
2024-05-11 23:48:00 +0800

    inscriptions page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
+import Loading from "@/components/Loading";
 import { log } from "@/utils";
 import { fetchRecords } from "@/utils/api";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import {formatDistanceToNow} from 'date-fns';
 
 enum ASC20Operation {
-  Deploy='deploy',
-  Mint='mint',
-  Transfer='transfer',
-  List='list',
+  Deploy = 'deploy',
+  Mint = 'mint',
+  Transfer = 'transfer',
+  List = 'list',
 }
 
 interface IASC20Protocol {
@@ -21,6 +23,7 @@ interface IASC20Protocol {
 
 type ASC20Record = IASC20Protocol & {
   id: number;
+  timestamp: number
 }
 
 const formatContent = (item: ASC20Record) => {
@@ -30,7 +33,7 @@ const formatContent = (item: ASC20Record) => {
     tick: item.tick,
     amt: item.amount,
   }
-  type Protocal = typeof o & {limit?: number}
+  type Protocal = typeof o & { limit?: number }
   const o1 = o as Protocal
   if (item.operation === ASC20Operation.Deploy) {
     o1.limit = item.limit
@@ -41,41 +44,54 @@ const formatContent = (item: ASC20Record) => {
 
 export default function Home() {
   const [list, setList] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     handleRefresh()
   }, [])
-  
+
   const handleRefresh = () => {
+    setLoading(true)
     fetchRecords(100000000).then(data => {
       setList(data.data)
+      setLoading(false)
+    }).catch(err => {
+      setLoading(false)
     })
   }
 
   return (
     <div>
-      <div className="flex justify-between items-center px-4 py-4">
+      <div className="flex justify-between items-center px-4 pt-8 pb-4">
         <p className="text-primary text-xl font-bold">Latest Inscriptions</p>
-        <section className="flex items-center cursor-pointer" onClick={handleRefresh}>
+        <button className="flex items-center cursor-pointer" onClick={handleRefresh} disabled={loading}>
           <div className="mr-2 text-primary">Refresh</div>
           <Image src="/sync.svg" alt="refresh" width={18} height={18} />
-        </section>
+        </button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 justify-between p-4">
-        {list.map((item: ASC20Record, index) => {
-          return (
-            <div key={item.id} className="card bg-stone-100 min-w-56 ring-2 ring-base-300 hover:ring-primary cursor-pointer">
-              <div className="card-body w-full h-70 p-0">
-                <pre className="p-6 h-full">
-                  {formatContent(item)}
-                </pre>
-                <div className="bg-stone-200 rounded-bl-2xl rounded-br-2xl py-4 px-6">465</div>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      {loading && <Loading />}
+
+      {
+        !loading && (
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 justify-between p-4">
+            {list.map((item: ASC20Record, index) => {
+              return (
+                <div key={item.id} className="card bg-stone-100 min-w-56 ring-2 ring-base-300 hover:ring-primary cursor-pointer">
+                  <div className="card-body w-full h-70 p-0">
+                    <pre className="p-6 h-full">
+                      {formatContent(item)}
+                    </pre>
+                    <div className="bg-stone-200 rounded-bl-2xl rounded-br-2xl py-4 px-6">
+                      {formatDistanceToNow(item.timestamp * 1000)}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )
+      }
     </div>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen -translate-y-32">
+      <span className="loading loading-dots loading-lg text-primary"></span>
+    </div>
+  );
+}
