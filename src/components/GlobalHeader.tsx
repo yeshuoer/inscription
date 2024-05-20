ef61c3a148db9220817dd662a61960606fd826e1
yeshuoer <xlsapple@outlook.com>
2024-05-20 21:03:55 +0800

    add progressbar

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 export default function GlobalHeader() {
   const pathname = usePathname()
@@ -13,26 +14,32 @@ export default function GlobalHeader() {
     return pathname.startsWith(link) ? 'tab tab-active' : 'tab'
   }
 
-  return <header className='box-border px-6 fixed left-0 top-0 z-10 w-full border-b-2 bg-base-100'>
-    <div className="navbar">
-      <div className="flex-1">
-        <Link href='/' className='text-xl font-bold italic text-primary flex items-center'>
-          <Image src='/logo.png' alt='logo' width={48} height={48} />
-          Inscription Market
-        </Link>
+  return <>
+    <ProgressBar 
+      color="#81192a"
+      options={{ showSpinner: false }}
+    />
+    <header className='box-border px-6 fixed left-0 top-0 z-10 w-full border-b-2 bg-base-100'>
+      <div className="navbar">
+        <div className="flex-1">
+          <Link href='/' className='text-xl font-bold italic text-primary flex items-center'>
+            <Image src='/logo.png' alt='logo' width={48} height={48} />
+            Inscription Market
+          </Link>
+        </div>
+
+        <ul role='tablist' className='tabs tabs-boxed flex-1'>
+          <li role='tab' className={activeClassName('/inscription')}><Link href="/inscription">Inscriptions</Link></li>
+          <li role='tab' className={activeClassName('/token')}><Link href="/token">Tokens</Link></li>
+          <li role='tab' className={activeClassName('/market')}><Link href="/market">Marketplace</Link></li>
+          <li role='tab' className={activeClassName('/personal')}><Link href="/personal/tokens">Personal</Link></li>
+        </ul>
+
+        <div className='flex-1 justify-end'>
+          <ConnectButton showBalance={false} />
+        </div>
       </div>
 
-      <ul role='tablist' className='tabs tabs-boxed flex-1'>
-        <li role='tab' className={activeClassName('/inscription')}><Link href="/inscription">Inscriptions</Link></li>
-        <li role='tab' className={activeClassName('/token')}><Link href="/token">Tokens</Link></li>
-        <li role='tab' className={activeClassName('/market')}><Link href="/market">Marketplace</Link></li>
-        <li role='tab' className={activeClassName('/personal')}><Link href="/personal/tokens">Personal</Link></li>
-      </ul>
-
-      <div className='flex-1 justify-end'>
-        <ConnectButton showBalance={false} />
-      </div>
-    </div>
-
-  </header>
+    </header>
+  </>
 }
